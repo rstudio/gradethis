074888d4e8ec446d769418fc69802c71ee85ec4a
Garrett Grolemund <grolemund@gmail.com>
2018-03-28 16:34:11 -0500

    Adds documentation for grade_learnr()

--- R/grade_learnr.R
+++ R/grade_learnr.R
@@ -1,3 +1,35 @@
+#' grade_learnr
+#'
+#' A checker function to use with learnr
+#'
+#' For exercise checking, learnr tutorials require a function that learnr can
+#' use in the background to run the code in each "-check" chunk and to format
+#' the results into a format that learnr can display. The function must accept a
+#' specific set of inputs and return a specific type of output. Users are not
+#' intended to use the function themselves, but to pass it to the
+#' \code{exercise.checker} knitr chunk option within the setup chunk of the
+#' tutorial.
+#'
+#' The grader package provides \code{grade_learnr()} for this purpose. To enable
+#' exercise checking in your learnr tutorial, set
+#' \code{tutorial_options(exercise.checker = grade_learnr)} in the setup chunk
+#' of your tutorial. 
+#' 
+#' Run \code{grading_demo()} to see an example learnr document
+#' that uses \code{grade_learnr()}.
+#'
+#' @param label Label for exercise chunk
+#' @param solution_code R code submitted by the user
+#' @param user_code 	Code provided within the “-solution” chunk for the exercise.
+#' @param check_code 	Code provided within the “-check” chunk for the exercise.
+#' @param envir_result 	The R environment after the execution of the chunk.
+#' @param evaluate_result The return value from the \code{evaluate::evaluate} function.
+#' @param ... Unused (include for compatibility with parameters to be added in the future)
+#'
+#' @return An R list which contains several fields indicating the result of the check.
+#' @export
+#'
+#' @examples
 grade_learnr <- function(label = NULL,
                          solution_code = NULL,
                          user_code = NULL,
