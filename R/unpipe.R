6895c9642e5aceb5ce0bf3b8feaa2ea648abd01c
Garrett Grolemund <grolemund@gmail.com>
2018-06-19 15:50:52 -0500

    Adds unpipe_all() function

--- R/unpipe.R
+++ R/unpipe.R
@@ -7,31 +7,43 @@
 #' contain a top-level call to \%>\%, \code{unpipe()} returns it as is.
 #'
 #' @param code a quoted piece of code
+#' @export
 unpipe <- function(code) {
-  
+
   # Ceci n'est pas une pipe
-  if (!is.call(code)) return(code)
-  if (as.character(code[[1]]) != "%>%") return(code)
-  
+  if (!is_pipe(code)) return(code)
+
   # une pipe
   lhs <- code[[2]]
   rhs <- code[[3]]
-  
+
   if (length(rhs) == 1) {
     rhs[[2]] <- lhs
     return(rhs)
   }
-  
+
   dot <- purrr::map_lgl(as.list(rhs), is_dot)
   if (any(dot)) {
     rhs[[which(dot)]] <- lhs
   } else {
-    rhs[3:(length(rhs) + 1)] <- rhs[2:length(rhs)]
-    rhs[[2]] <- lhs
+    rhs <- as.call(c(list(rhs[[1]], lhs), as.list(rhs[2:length(rhs)])))
   }
   rhs
 }
 
 is_dot <- function(name) {
   length(name) == 1 && as.character(name) == "."
+}
+
+#' @export
+unpipe_all <- function(code) {
+  if (length(code) == 1) return(code)
+  code <- as.call(purrr::map(as.list(code), unpipe_all))
+  unpipe(code)
+}
+
+unpipe_lhs <- function(code) {
+  if (!is_pipe(code)) return(code)
+  if (is_pipe(code[[2]])) code[[2]] <- unpipe_all(code[[2]])
+  unpipe(code)
 }
--- tests/testthat/test_unpipe.R
+++ tests/testthat/test_unpipe.R
@@ -8,16 +8,50 @@ test_that("unpipe() strips off the top level of piping", {
   expect_equal(unpipe(pipe), func)
 })
 
-test_that("unpipe() recognizes . syntax", {
+test_that("unpipe() recognizes top level . syntax", {
   pipe2 <- quote(iris %>% lm(Sepal.Length ~ Sepal.Width, data = .))
   func2 <- quote(lm(Sepal.Length ~ Sepal.Width, data = iris))
   
   expect_equal(unpipe(pipe2), func2)
 })
 
-
 test_that("unpipe() does not alter unpiped code", {
   func2 <- quote(lm(Sepal.Length ~ Sepal.Width, data = iris))
   
   expect_equal(unpipe(func2), func2)
+})
+
+test_that("unpipe() preserves argument names", {
+  
+  expect_equal(unpipe(quote(iris %>% mutate(species = Species %>% tolower()))), 
+               quote(mutate(iris, species = Species %>% tolower())))
+})
+
+test_that("unpipe_all() completely unpipes code with multiple pipes", {
+  
+  expect_equal(unpipe_all(quote(a %>% b() %>% c())), quote(c(b(a))))
+  expect_equal(unpipe_all(quote(a %>% b())), quote(b(a)))
+  expect_equal(unpipe_all(quote(a)), quote(a))
+  expect_equal(unpipe_all(quote(iris %>% pluck("Sepal.Length") %>% mean())),
+               quote(mean(pluck(iris, "Sepal.Length"))))
+})
+
+test_that("unpipe_all() unpipes pipes that occur within arguments", {
+  
+  expect_equal(unpipe_all(quote(iris %>% mutate(species = Species %>% tolower()))),
+               quote(mutate(iris, species = tolower(Species))))
+})
+
+
+test_that("unpipe_all() recognizes top level . syntax", {
+  pipe2 <- quote(iris %>% lm(Sepal.Length ~ Sepal.Width, data = .) %>% summary())
+  func2 <- quote(summary(lm(Sepal.Length ~ Sepal.Width, data = iris)))
+  
+  expect_equal(unpipe_all(pipe2), func2)
+})
+
+test_that("unpipe_all does not alter unpiped code", {
+  func2 <- quote(lm(Sepal.Length ~ Sepal.Width, data = iris))
+  
+  expect_equal(unpipe_all(func2), func2)
 })
