974ed1d9656d42ff25c7ad360ae656a50a3d3965
Garrett Grolemund <grolemund@gmail.com>
2018-06-22 13:38:59 -0500

    Updates order_calls to preserve argument names

--- R/order_calls.R
+++ R/order_calls.R
@@ -6,42 +6,66 @@
 #'
 order_calls <- function(code) {
   if (is.name(code) ||
-    is.call(code) ||
-    is.atomic(code)) {
+      is.call(code) ||
+      is.atomic(code)) {
     code <- list(code)
   }
   if (is.call(code[[1]]) && length(code[[1]]) != 1) {
     code[[1]] <- standardize_call(code[[1]])
-    code <- c(pre_pipe(code[[1]]), code[-1])
+    code <- c(pre_pipe(code[[1]], name = names(code[1])), code[-1])
     code <- order_calls(code)
   }
   code
 }
 
-pre_pipe <- function(code) {
+pre_pipe <- function(code, name = "") {
   if (is.call(code)) {
-    list(code[[2]], code[c(1, 3)])
+    new <- list(code[[2]], code[c(1, 3)])
+    
+    name2 <- names(code)[[2]]
+    if (is.null(name2) || name2 == "") arg_name <- ""
+    else arg_name <- name2
+    if (is.null(name)) name <- ""
+    names(new) <- c(arg_name, name)
+    
+    new
+  } else {
+    code
   }
 }
 
-repipe <- function(lst) {
+repipe <- function(lst, .call = FALSE) {
   text <- purrr::map(lst, deparse)
+  text <- purrr::reduce(text, paste, sep = " %>% ")
+  text <- gsub("\\(NULL\\)", "()", text)
+  
+  if(.call) parse(text = text)[[1]]
+  else text
+}
 
-  # alternatively use accumulate for step by step checking
-  text <- purrr::reduce(text, paste, sep = "%>%")
-  eval(parse(text = text))
+renest <- function(lst, .call = FALSE) {
+  
+  nest <- function(a, b) {
+    if (is.call(b)) {
+      # is b[[2]] not a NULL placeholder?
+      if (!is.null(b[[2]]) || length(b) != 2) b[3:(length(b) + 1)] <- b[2:length(b)]
+      b[[2]] <- a
+    }
+    b
+  }
+  
+  code <- purrr::reduce(lst, nest)
+  if (.call) code
+  else deparse(code)
 }
 
 # Modified from pryr::standardise_call
 standardize_call <- function(call, env = parent.frame()) {
   stopifnot(is.call(call))
   f <- eval(call[[1]], env)
-  if (is.primitive(f)) {
-    if (!is.null(args(f))) {
-      return(match.call(args(f), call))
-    } else {
-      return(call)
-    }
+  if (!is.null(args(f))) {
+    match.call(args(f), call)
+  } else {
+    call
   }
-  match.call(f, call)
 }
