2f6c67ac28f0aa7aff4b102a423b58d01b7b0e46
Garrett Grolemund <grolemund@gmail.com>
2018-06-22 12:02:14 -0500

    Adds consistent message generators that are easy to use with testthat for the eight common types of mistakes.

--- R/message_generators.R
+++ R/message_generators.R
@@ -1,48 +1,97 @@
-expected <- function(this, .name = NULL) {
-  if (is.null(.name)) {
-    return(glue::glue(
-      "I expected your code to include {deparse(this)}. You may have ",
-      "referred to it in a different way, or left out an important argument name. ",
-      "Please try again."
-    ))
-  } else {
-    return(glue::glue(
-      "I expected your code to include {.name} = {deparse(this)}. ",
-      "You may have referred to it in a different way, or left out an important ",
-      "argument name. Please try again."
-    ))
-  }
+# Each case should have a function that takes both strings and quotes, and hence
+# is easy to drop into testthat. ("that" always refers to correct/solution code,
+# because it is not at hand in the submission. "this" always refers to
+# incorrect/user code, which is always at hand). Cases:
+
+# missing value
+missing_value <- function(that) {
+  that <- prep(that)
+  glue::glue("I expected your code to include {that}. You may have ",
+             "referred to it in a different way, or left out an important ",
+            "argument name. {sample(.encourage, 1)}")
 }
 
-did_not_expect <- function(that, .name = NULL) {
-  if (is.null(.name)) {
-    return(glue::glue(
-      "I did not expect your code to include {deparse(that)}. ",
-      "You may have included an unnecessary value, or you may have left ",
-      "out an important argument name. Please try again."
-    ))
-  } else {
-    return(glue::glue(
-      "I did not expect your code to include {.name} = {deparse(that)}. ",
-      "You may have included an unnecessary value, or you may have used the wrong ",
-      "argument name. Please try again."
-    ))
-  }
+# surplus value
+surplus_value <- function(this) {
+  this <- prep(this)
+  glue::glue("I did not expect your code to include {this}. ",
+             "You may have included an unnecessary value, or you may have left ",
+             "out an important argument name. {sample(.encourage, 1)}")
 }
 
-does_not_match <- function(user, solution, .name = NULL) {
-  if (length(solution) > 1) solution <- solution[[1]]
-  if (length(user) > 1) user <- user[[1]]
+
+# wrong value
+wrong_value <- function(this, that) {
+  this <- prep(this)
+  that <- prep(that)
+  glue::glue("I expected {that} where you wrote {this}. {sample(.encourage, 1)}")
+}
+
+# missing_argument
+missing_argument <- function(this_call, that_name) {
+  this_call <- prep(this_call)
+  that_name <- prep(that_name)
   
-  if (is.null(.name)) {
-    return(glue::glue(
-      "I expected {deparse(solution)} where you wrote {deparse(user)}. ",
-      "Please try again."
-    ))
-  } else {
-    return(glue::glue(
-      "I expected {.name} = {deparse(solution)} where you wrote ",
-      "{.name} = {deparse(user)}. Please try again."
-    ))
-  }
+  glue::glue("Your call to {this_call} should include ",
+             "an argument named {that_name}. You may have ",
+             "referred to it in a different way, or left ",
+             "out an important argument. {sample(.encourage, 1)}")
 }
+
+# surplus argument
+surplus_argument <- function(this_call, this_name, this) {
+  this_call <- prep(this_call)
+  this_name <- prep(this_name)
+  this      <- prep(this)
+  
+  glue::glue("I did not expect your call to {this_call} to ",
+             "include the argument {this_name} = {this}. You ",
+             "may have included an unnecessary argument, or you ",
+             "may have left out or misspelled an important ",
+             "argument name. {sample(.encourage, 1)}")
+}
+
+# missing call
+missing_call <- function(that_call, this) {
+  that_call <- prep(that_call)
+  this <- prep(this)
+  
+  glue::glue("I expected you to call {that_call} on {this}. {sample(.encourage, 1)}")
+}
+
+# surplus call
+surplus_call <- function(this_call, this) {
+  that_call <- prep(that_call)
+  this <- prep(this)
+  
+  glue::glue("I did not expect you to call {this_call} ",
+             "(or anything else) on {this}. {sample(.encourage, 1)}")
+}
+
+# wrong call
+wrong_call <- function(this_call, this, that_call) {
+  this_call <- prep(this_call)
+  this      <- prep(this)
+  that_call <- prep(that_call)
+  
+  glue::glue("I expected you to call {that_call} on {this} ",
+             "(instead of {this_call}). {sample(.encourage, 1)}")
+}
+
+prep <- function(text) {
+  if (is.name(text) || is.call(text)) return(deparse(text))
+  text
+}
+
+# Encouragement messages
+.encourage <- c("Please try again.",
+                "Give it another try.",
+                "Let's try it again.",
+                "Try it again; next time's the charm!",
+                "Don't give up now, try it one more time.",
+                "But no need to fret, try it again.",
+                "Try it again. I have a good feeling about this.",
+                "Try it again. You get better each time.",
+                "Try it again. Perseverence is the key to success.",
+                "That's okay: you learn more from mistakes than successes. Let's do it one more time.")
+                
