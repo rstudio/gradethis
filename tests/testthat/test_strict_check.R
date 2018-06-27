9b2377200a1e3e2532245a5c8f5447f88f7c8c81
Garrett Grolemund <grolemund@gmail.com>
2018-06-27 13:46:08 -0500

    Adds tests for strict_check()

--- tests/testthat/test_strict_check.R
+++ tests/testthat/test_strict_check.R
@@ -1,16 +1,48 @@
 library(tidyverse)
 context("Strict check")
 
+# these tests are largely redundant exercises that have been tested against detect_mistakes()
+
 test_that("Spots differences in atomics", {
-  expect_match(strict_check(user = "1", solution = "1"), "Correct!")
-  expect_equal(strict_check(user = quote(1), solution = quote(2)), does_not_match(quote(1), quote(2)))
+  
+  user <- quote(1)
+  solution <- quote(1)
+  expect_null(
+              strict_check(user = user, solution = solution)
+              )
+  
+  user <- quote(1)
+  solution <- quote(2)
+  expect_equal(
+               strict_check(user = user, solution = solution)
+               ,
+               wrong_value(this = quote(1), that = quote(2))
+               )
 })
 
 test_that("Spots differences in names", {
-  y <- 5
-  expect_equal(strict_check(user = quote(x), solution = quote(x)), "Correct!")
-  expect_equal(strict_check(user = quote(x), solution = quote(y)), does_not_match(quote(x), quote(y)))
-  expect_equal(strict_check(user = quote(5), solution = quote(y)), does_not_match(quote(5), quote(y)))
+  
+  user <- quote(x)
+  solution <- quote(y)
+  expect_equal(
+    strict_check(user = user, solution = solution)
+    ,
+    wrong_value(this = quote(x), that = quote(y))
+  )
+  
+  user <- quote(x)
+  solution <- quote(x)
+  expect_null(
+    strict_check(user = user, solution = solution)
+  )
+  
+  user <- quote(5)
+  solution <- quote(y)
+  expect_equal(
+    strict_check(user = user, solution = solution)
+    ,
+    wrong_value(this = quote(5), that = quote(y))
+  )
 })
 
 test_that("Spots differences in calls", {
@@ -18,11 +50,32 @@ test_that("Spots differences in calls", {
   b <- quote(map(vecs, mean, na.rm = TRUE))
   c <- quote(map(lists, mean))
   d <- quote(map(vecs, mean))
+  
+  expect_null(
+    strict_check(user = a, solution = a)
+  )
+  
+  expect_equal(
+    strict_check(user = a, solution = b)
+    ,
+    wrong_value(this = quote(lists), that = quote(vecs))
+  )
+  
+  expect_equal(
+    strict_check(user = a, solution = c)
+    ,
+    surplus_argument(this_call = "map()", 
+                     this_name = "na.rm",
+                     this = quote(TRUE))
+  )
 
-  expect_equal(strict_check(user = a, solution = a), "Correct!")
-  expect_equal(strict_check(user = a, solution = b), does_not_match(quote(lists), quote(vecs)))
-  expect_equal(strict_check(user = a, solution = c), did_not_expect(a[[4]], .name = "na.rm"))
-  expect_equal(strict_check(user = c, solution = a), expected(a[[4]], .name = "na.rm"))
+  expect_equal(
+    strict_check(user = c, solution = a)
+    ,
+    missing_argument(this_call = "map()", 
+                     that_name = "na.rm",
+                     that = quote(TRUE))
+  )
 })
 
 test_that("Mentions only first non-matching element", {
@@ -30,42 +83,93 @@ test_that("Mentions only first non-matching element", {
   x <- quote(log(1))
   y <- quote(sqrt(log(2)))
   z <- quote(sqrt(log(1)))
+  
+  expect_null(
+    strict_check(user = w, solution = w)
+  )
+  
+  expect_equal(
+    strict_check(user = w, solution = z)
+    ,
+    wrong_value(this = quote(1), that = quote(sqrt()))
+  )
+  
+  expect_equal(
+    strict_check(user = x, solution = z)
+    ,
+    wrong_value(this = "log(1)", that = quote(sqrt()))
+  )
+  
+  expect_equal(
+    strict_check(user = y, solution = z)
+    ,
+    wrong_value(this = "2", that = quote(1))
+  )
 
-  expect_equal(strict_check(user = z, solution = z), "Correct!")
-  expect_equal(strict_check(user = w, solution = z), does_not_match(w, z))
-  expect_equal(strict_check(user = x, solution = z), does_not_match(x, z))
-  expect_equal(strict_check(user = y, solution = z), does_not_match(quote(2), quote(1)))
 })
 
 test_that("Spots differences in argument names", {
   a <- quote(mean(1:10, trim = 1, na.rm = TRUE))
   b <- quote(mean(1:10, 1, TRUE))
   c <- quote(mean(1:10, cut = 1, na.rm = TRUE))
+  
+  expect_null(
+    strict_check(user = a, solution = a)
+  )
+  
+  expect_null(
+    strict_check(user = b, solution = a)
+  )
+  
+  expect_equal(
+    strict_check(user = c, solution = a)
+    ,
+    wrong_value(this = quote(1), this_name = "cut",
+                that = quote(1), that_name = "trim")
+  )
 
-  expect_equal(strict_check(user = a, solution = a), "Correct!")
-  expect_equal(strict_check(user = b, solution = a), expected(a[[3]], .name = "trim"))
-  expect_equal(strict_check(user = c, solution = a), did_not_expect(c[[3]], .name = "cut"))
 })
 
-test_that("Ignore differences in argument positions", {
-  a <- quote(mean(1:10, trim = 1, na.rm = TRUE))
-  b <- quote(mean(1:10, na.rm = TRUE, trim = 1))
-  c <- quote(mean(1:10, na.rm = TRUE, 1))
-  d <- quote(mean(1:10))
+test_that("Ignore differences in argument positions (for non ... arguments)", {
+  a <- quote(round(x = pi, digits = 2))
+  b <- quote(round(pi, digits = 2))
+  c <- quote(round(2, x = pi))
+  d <- quote(round(digits = 2, x = pi))
 
-  expect_equal(strict_check(user = a, solution = a), "Correct!")
-  expect_equal(strict_check(user = b, solution = a), "Correct!")
-  expect_equal(strict_check(user = c, solution = a), expected(a[[3]], .name = "trim"))
-  expect_equal(strict_check(user = d, solution = a), expected(a[[3]], .name = "trim"))
-  expect_equal(strict_check(user = a, solution = d), did_not_expect(a[[3]], .name = "trim"))
+  expect_null(
+    strict_check(user = b, solution = a)
+  )
+  
+  expect_null(
+    strict_check(user = c, solution = a)
+  )
+  
+  expect_null(
+    strict_check(user = d, solution = a)
+  )
+  
+  expect_null(
+    strict_check(user = a, solution = d)
+  )
+  
 })
 
 test_that("Returns intelligent error when no solution code", {
-  expect_error(strict_check(), "No solution is provided for this exercise.")
+  
+  expect_error(
+    strict_check()
+    , 
+    "No solution is provided for this exercise."
+  )
+  
 })
 
 test_that("Returns intelligent error when no user code", {
-  expect_error(strict_check(solution = quote(5)), "I didn't receive your code. Did you write any?")
+  expect_error(
+    strict_check(solution = quote(5))
+    , 
+    "I didn't receive your code. Did you write any?"
+    )
 })
 
 test_that("Spot differences when pipes are involved", {
@@ -77,25 +181,16 @@ test_that("Spot differences when pipes are involved", {
   pipe3 <- quote(iris %>% lm(Sepal.Length ~ Sepal.Width, data = .))
   func3 <- quote(lm(Sepal.Length ~ Sepal.Width, data = iris))
   
-  expect_equal(strict_check(user = func,  solution = pipe), "Correct!")
-  expect_equal(strict_check(user = func1, solution = pipe), "Correct!")
-  expect_equal(strict_check(user = pipe,  solution = func), "Correct!")
-  expect_equal(strict_check(user = pipe,  solution = func1), "Correct!")
-  expect_equal(strict_check(user = pipe,  solution = pipe), "Correct!")
-  expect_equal(strict_check(user = func,  solution = func1), "Correct!")
-  expect_equal(strict_check(user = func1, solution = func1), "Correct!")
-  
-  expect_equal(strict_check(user = pipe1, solution = pipe), does_not_match(quote(Petal.Length), quote(Sepal.Length)))
-  expect_equal(strict_check(user = pipe1, solution = func), does_not_match(quote(Petal.Length), quote(Sepal.Length)))
-  expect_equal(strict_check(user = pipe1, solution = func1), does_not_match(quote(Petal.Length), quote(Sepal.Length)))
-  expect_equal(strict_check(user = pipe2, solution = pipe), does_not_match(quote(arrange), quote(filter)))
-  expect_equal(strict_check(user = pipe2, solution = func), does_not_match(quote(arrange), quote(filter)))
-  expect_equal(strict_check(user = pipe2, solution = func1), does_not_match(quote(arrange), quote(filter)))
-  
-  
-  expect_equal(strict_check(user = func3, solution = pipe3), "Correct!")
-  expect_equal(strict_check(user = pipe3, solution = func3), "Correct!")
-  expect_equal(strict_check(user = pipe3, solution = pipe3), "Correct!")
+  expect_null(strict_check(user = func,  solution = pipe))
+  expect_null(strict_check(user = func1, solution = pipe))
+  expect_null(strict_check(user = pipe,  solution = func))
+  expect_null(strict_check(user = pipe,  solution = func1))
+  expect_null(strict_check(user = pipe,  solution = pipe))
+  expect_null(strict_check(user = func,  solution = func1))
+  expect_null(strict_check(user = func1, solution = func1))
+  expect_null(strict_check(user = func3, solution = pipe3))
+  expect_null(strict_check(user = pipe3, solution = func3))
+  expect_null(strict_check(user = pipe3, solution = pipe3))
   
 })
 
