81a11fa2cc981dd207503b233fb561109d286217
Barret Schloerke <schloerke@gmail.com>
2019-04-19 11:14:21 -0400

    check result should not check code

--- R/check_result.R
+++ R/check_result.R
@@ -1,50 +1,48 @@
 
+# TODO document
 #' @export
-#' @importFrom rlang get_expr
 check_result <- function(
   results,
-  correct = NULL,
-  incorrect = NULL,
-  strict = correct,
+  correct = "{paste0(random_praise(), if (!is.null(message)) paste0(\" \", message))}",
+  incorrect = "{paste0(if(!is.null(message)) paste0(message, \" \"), random_encourage())}",
+  # TODO check for empty answer
   empty_msg = "I did not notice a result. Does your code return one?",
-  solution = NULL, user = NULL # provided by `grade_learnr`
+  user = NULL, # provided by `grade_learnr`
+  solution = NULL,
+  ... # ignored / extra params
 ) {
+  chkm8_class(results, "grader_results")
+  chkm8_single_character(correct)
+  chkm8_single_character(incorrect)
+  chkm8_single_character(empty_msg)
 
-  same_info <- code_is_same(user = user, solution = solution)
-  if (same_info$correct) {
-    return(result(
-      x = solution,
-      message = strict,
-      correct = TRUE
-    ))
-  }
-
-  if (is.null(solution)) {
-    stop("'solution' not provided")
-  }
-  user_val <- rlang::eval_tidy(user)
-  solution_val <- rlang::eval_tidy(solution)
-  if (identical(user_val, solution_val)) {
-    print("user_val == solution_val")
-    return(result(
-      x = solution_val,
-      message = correct,
-      correct = TRUE
-    ))
-  }
+  user_val <- get_user_code(user)
   for (resu in results) {
-    if (!inherits(resu, "grader_result")) {
-      stop("All results must be produced by 'grader::result'")
-    }
     if (identical(resu$x, user_val)) {
-      print("user_val == ans$x")
-      return(ans)
+      message <- glue::glue_data(
+        list(
+          message = resu$message
+        ),
+        ifelse(resu$correct, correct, incorrect)
+      )
+
+      # print("user_val == ans$x")
+      return(result(
+        x = resu$x,
+        message = message,
+        correct = resu$correct
+      ))
     }
   }
-  print("not found")
+  # print("not found")
   return(result(
-    x = structure("answer not found", class = "exercise_answer_not_found"),
-    message = incorrect,
+    x = structure("answer not found", class = c("exercise_answer_not_found", "character")),
+    message = glue::glue_data(
+      list(
+        message = NULL
+      ),
+      incorrect
+    ),
     correct = FALSE
   ))
 }
