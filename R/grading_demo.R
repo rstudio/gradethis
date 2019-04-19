68cf5883d5cd46283fac8f15ac4c6edf44ecfc9d
Barret Schloerke <schloerke@gmail.com>
2019-04-19 11:21:43 -0400

    open file _url_ if rstudio isn't available

--- R/grading_demo.R
+++ R/grading_demo.R
@@ -2,7 +2,7 @@
 #'
 #' If you are using the RStudio IDE, \code{grading_demo()} opens an example learnr
 #' file that demonstrates how to use the grader package to check student code.
-#' 
+#'
 #' The tutorial sets the learnr exercise. checker option to
 #' \code{grade_learnr()} in the document's setup chunk. It then uses
 #' \code{strict_check()} to check the single exercise in the tutorial. To use
@@ -11,10 +11,15 @@
 #' \code{-check}. Call \code{strict_check()} in that chunk. To ensure that
 #' \code{strict_check()} can provide formative feedback, also provide the
 #' solution code in a chunk suffixed \code{-solution}.
-#' 
+#'
 #' If you are not using RStudio IDE, you can access the demo file at \code{system.file("extdata", "grading-demo/grading-demo.Rmd", package = "grader")}.
 #'
 #' @export
 grading_demo <- function() {
-  rstudioapi::navigateToFile(system.file("tutorials", "grading-demo/grading-demo.Rmd", package = "grader"))
+  grading_demo_path <- system.file("tutorials", "grading-demo/grading-demo.Rmd", package = "grader")
+  if (rstudioapi::isAvailable()) {
+    rstudioapi::navigateToFile(grading_demo_path)
+  } else {
+    browseURL(paste0('file://', grading_demo_path))
+  }
 }
