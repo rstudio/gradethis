3bbbc14a270c25d24936badfc49be6c51684d619
Barret Schloerke <barret@rstudio.com>
2019-08-06 10:32:42 -0400

    add \link

--- man-roxygen/grader_args.R
+++ man-roxygen/grader_args.R
@@ -1,4 +1,4 @@
-#' @param grader_args A list of parameters passed to \code{grader} functions (provided by \code{grade_learnr}).
+#' @param grader_args A list of parameters passed to \code{grader} functions (provided by \code{\link{grade_learnr}}).
 #'   This contains: \describe{
 #'    \item{\code{user_quo}}{Quoted R code submitted by the user.  Ex: \code{rlang::\link[rlang]{quo}(1)} }
 #'    \item{\code{solution_quo}}{[Optional] Quoted solution R code provided by the \code{*-solution} chunk for an exercise.}
