867437e3afd781db63201239b81c7db05c660b58
Daniel Chen <chendaniely@users.noreply.github.com>
2019-08-06 10:32:26 -0400

    add \code brackets

--- man-roxygen/glue_correct.R
+++ man-roxygen/glue_correct.R
@@ -1,2 +1,2 @@
 #' @param glue_correct A glue string that returns the final correct message displayed.
-#'    Defaults to getOption("gradethis_glue_correct").
+#'    Defaults to \code{getOption("gradethis_glue_correct")}.
