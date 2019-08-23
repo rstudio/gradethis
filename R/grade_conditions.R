2323cb590924866686da46e393227f5c3cf480a4
Daniel Chen <chendaniely@gmail.com>
2019-08-22 23:43:15 -0400

    put back grade_conditions examples and regenerate docs

--- R/grade_conditions.R
+++ R/grade_conditions.R
@@ -37,6 +37,24 @@
 #' @export
 #' @examples
 #' \dontrun{gradethis_demo()}
+#'
+#' example_function <- function(x){
+#'   return(x + 1)
+#' }
+#' grade_conditions(
+#'   pass_if(~ .result(3) == 4),
+#'   pass_if(~ .result(10) == 11),
+#'   grader_args = list(),
+#'   learnr_args = list(last_value = example_function, envir_prep = new.env())
+#' )
+#'
+#' grade_conditions(
+#'   pass_if(~ .result(3) == 4),
+#'   fail_if(~ .result(10) == 11),
+#'   grader_args = list(),
+#'   learnr_args = list(last_value = example_function, envir_prep = new.env())
+#' )
+#'
 grade_conditions <- function(
   ...,
   correct = NULL,
