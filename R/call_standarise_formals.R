83b750081738dc269b7a76b1934f0cbfe0550861
Garrett Grolemund <grolemund@gmail.com>
2020-03-13 16:42:59 -0400

    Fixes #91.

--- R/call_standarise_formals.R
+++ R/call_standarise_formals.R
@@ -1,17 +1,19 @@
 call_standardise_formals <- function(code, env = rlang::current_env()) {
-  # browser()
   
-  # try to catch invalid function, i.e., corrupt language object
+  # look up the function definition both to use and 
+  # to spot invalid language objects
   tryCatch({
     fxn <- rlang::call_fn(code, env = env)
   }, error = function(e) {
     return(code)
   })
-  if (!exists("fxn")) {return(code)} ## some reason the above tryCatch doesn't go to the error part
+  
+  ## some reason the above tryCatch doesn't go to the error part
+  if (!exists("fxn")) {return(code)} 
   if(class(fxn) != "function") {return(code)}
   
-  # standarise, but dont bother trying to fill out default formals
-  # for primitives like mean, unable to distinguish between mean and mean.default
+  # standarise, but dont bother trying to fill out default formals for
+  # primitives like mean, unable to distinguish between mean and mean.default
   if (is_infix(code) || is.primitive(fxn)) {
     return(rlang::call_standardise(code))
   }
@@ -19,11 +21,22 @@ call_standardise_formals <- function(code, env = rlang::current_env()) {
   forms <- rlang::fn_fmls(fxn)
   default_params <- forms[!vapply(forms, is.symbol, logical(1), USE.NAMES = FALSE)]
   
-  code_std <- rlang::call_standardise(code, env = env) # order and label existing params
+  # match.call in call_standardise will throw an error if code contains
+  # unmatched arguments (we'd prefer to inform the student), so add ... to
+  # function definition if it is not already there.
+  if (!("..." %in% names(forms))) {
+    formals(fxn) <- eval(rlang::call2("alist", !!!forms, ... = ))
+    env <- list(fxn)
+    names(env) <- as.character(code[[1]]) # can every non-primitve call be coerced ot character?
+    env <- as.environment(env)
+  }
   
+  # order and label existing params
+  code_std <- rlang::call_standardise(code, env = env)
   code_params <- rlang::call_args(code_std) # get arguments passed from user
   code_missing_default_args <- default_params[!names(default_params) %in% names(code_params)]
-  if (length(code_missing_default_args) == 0) {
+ 
+   if (length(code_missing_default_args) == 0) {
     return(code_std)
   }
   return(
@@ -31,3 +44,4 @@ call_standardise_formals <- function(code, env = rlang::current_env()) {
                             env = env)
   )
 }
+
