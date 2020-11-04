add6afba3c34f714b8e2ba67dfd5785b218f108d
vincent <vincent@thinkr.fr>
2020-11-04 19:32:08 +0100

    test allow_partial_matching works with multiple matches

--- tests/testthat/test-allow_partial_matching.R
+++ tests/testthat/test-allow_partial_matching.R
@@ -173,3 +173,79 @@ test_that("allow_partial_matching works errors multi arg type", {
   
   
 })
+
+test_that("allow_partial_matching works with multiple matches", {
+  
+  ff <-  function(ab, abc, abcd) { 1 }
+  
+  
+  solution_quo <- as.expression(quote(ff(ab = 1)))
+  user_quo <- as.expression(quote(ff(ab = 1)))
+  pmatch_FALSE <- grade_code(allow_partial_matching = FALSE,
+                             grader_args = list(
+                               user_quo = user_quo, 
+                               solution_quo = solution_quo
+                             ))
+  
+  expect_true(pmatch_FALSE$correct)
+  
+  
+  solution_quo <- as.expression(quote(ff(a = 1)))
+  user_quo <- as.expression(quote(ff(a = 1)))
+  pmatch_FALSE <- grade_code(allow_partial_matching = FALSE,
+                             grader_args = list(
+                               user_quo = user_quo, 
+                               solution_quo = solution_quo
+                             ))
+  
+  expect_true(pmatch_FALSE$correct)
+  
+  
+  
+  solution_quo <- as.expression(quote(ff(abc=3)))
+  user_quo <- as.expression(quote(ff(ab = 1,abc = 1)))
+  
+  set.seed(1)
+  pmatch_FALSE <- grade_code(allow_partial_matching = FALSE,
+                             grader_args = list(
+                               user_quo = user_quo, 
+                               solution_quo = solution_quo
+                             ))
+  set.seed(1)
+  pmatch_TRUE <- grade_code(allow_partial_matching = TRUE,
+                             grader_args = list(
+                               user_quo = user_quo, 
+                               solution_quo = solution_quo
+                             ))
+
+  
+  expect_false(pmatch_FALSE$correct)
+  expect_false(pmatch_TRUE$correct)
+  expect_equal(pmatch_FALSE,pmatch_TRUE)
+  
+  solution_quo <- as.expression(quote(ff(ab = 1,abc = 1)))
+  user_quo <- as.expression(quote(ff(ab = 1,abc = 1)))
+  pmatch_FALSE <- grade_code(allow_partial_matching = FALSE,
+             grader_args = list(
+               user_quo = user_quo, 
+               solution_quo = solution_quo
+             ))
+  
+  expect_true(pmatch_FALSE$correct)
+  
+  
+  solution_quo <- as.expression(quote(ff(ab = 1,abc = 1)))
+  user_quo <- as.expression(quote(ff(abc = 1,ab = 1)))
+  pmatch_FALSE <- grade_code(allow_partial_matching = FALSE,
+             grader_args = list(
+               user_quo = user_quo, 
+               solution_quo = solution_quo
+             ))
+  
+  expect_true(pmatch_FALSE$correct)
+  
+  
+  
+
+  
+})
