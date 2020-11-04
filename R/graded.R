bb8a162ae2c9bffe0d97954b795811045a463f9c
Barret Schloerke <schloerke@gmail.com>
2020-11-04 15:49:39 -0500

    pass/fail functions should received an env param

--- R/graded.R
+++ R/graded.R
@@ -33,30 +33,40 @@ is_graded <- function(x) {
 
 #' @rdname grade_result
 #' @export
-pass <- function(message = getOption("gradethis.pass", "Correct!")) {
-  graded(message = glue_with_env(parent.frame(1), message), correct = TRUE)
+pass <- function(message = getOption("gradethis.pass", "Correct!"), env = parent.frame()) {
+  graded(message = glue_with_env(env, message), correct = TRUE)
 }
 #' @rdname grade_result
 #' @export
-fail <- function(message = getOption("gradethis.fail", "Incorrect")) {
-  graded(message = glue_with_env(parent.frame(1), message), correct = FALSE)
+fail <- function(message = getOption("gradethis.fail", "Incorrect"), env = parent.frame()) {
+  graded(message = glue_with_env(env, message), correct = FALSE)
 }
 
 
 #' @export
-pass_if_equal <- function(y, message = getOption("gradethis.pass", "Correct!"), x = get(".result", envir = parent.frame())) {
-  grade_if_equal(x = x, y = y, message = message, correct = TRUE)
+pass_if_equal <- function(
+  y,
+  message = getOption("gradethis.pass", "Correct!"),
+  x = get(".result", envir = env),
+  env = parent.frame()
+) {
+  grade_if_equal(x = x, y = y, message = message, correct = TRUE, env = env)
 }
 #' @export
-fail_if_equal <- function(y, message = getOption("gradethis.fail", "Incorrect"), x = get(".result", envir = parent.frame())) {
-  grade_if_equal(x = x, y = y, message = message, correct = FALSE)
+fail_if_equal <- function(
+  y,
+  message = getOption("gradethis.fail", "Incorrect"),
+  x = get(".result", envir = env),
+  env = parent.frame()
+) {
+  grade_if_equal(x = x, y = y, message = message, correct = FALSE, env = env)
 }
-grade_if_equal <- function(x, y, message, correct, glue_env = parent.frame(2)) {
+grade_if_equal <- function(x, y, message, correct, env) {
   if (!identical(x, y)) {
     # not equal! quit early
     return()
   }
 
   # equal!
-  graded(message = glue_with_env(glue_env, message), correct = correct)
+  graded(message = glue_with_env(env, message), correct = correct)
 }
