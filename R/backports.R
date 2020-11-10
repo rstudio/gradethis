b436d9a34abb64f113f1bf4a66ffce49b996b076
Barret Schloerke <schloerke@gmail.com>
2020-11-10 17:29:59 -0500

    Make local copy of str2lang

--- R/backports.R
+++ R/backports.R
@@ -3,3 +3,6 @@
 str2expression <- function(text) {
   parse(text = text, keep.source = FALSE)
 }
+str2lang <- function(text) {
+  str2expression(text)[[1]]
+}
--- R/utils.R
+++ R/utils.R
@@ -12,7 +12,7 @@ is_infix <- function(x, infix_vals = .infixes) {
 
   tryCatch({
     if (is.character(x)) {
-      out <- call(x)
+      out <- str2lang(x)
     } else {
       out <- x
     }
