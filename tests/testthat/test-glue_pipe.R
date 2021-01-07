047b680bf04935edeb862d6f76af52be8ef1ffad
Garrick Aden-Buie <garrick@adenbuie.com>
2021-01-07 14:38:14 -0500

    Additional test that glue_pipe = "" also disables the message

--- tests/testthat/test-glue_pipe.R
+++ tests/testthat/test-glue_pipe.R
@@ -74,3 +74,35 @@ test_that("glue_pipe can be disabled by setting equal to NULL", {
     }
   )
 })
+
+test_that("glue_pipe can be disabled by setting equal to empty character string", {
+  user_code = "x %>% b()"
+  solution_code = "x %>% a()"
+  
+  with_options(
+    list(gradethis_glue_pipe = ""),
+    {
+      expect_equal(
+        expect_grade_code(
+          user_code = user_code,
+          solution_code = solution_code,
+          glue_incorrect = "INCORRECT",
+          is_correct = FALSE,
+          msg = NULL
+        )$message,
+        "INCORRECT"
+      )
+      
+      expect_equal(
+        expect_this_code(
+          user_code = user_code,
+          solution_code = solution_code,
+          incorrect = "INCORRECT",
+          is_correct = FALSE,
+          msg = NULL
+        )$message,
+        "INCORRECT"
+      )
+    }
+  )
+})
