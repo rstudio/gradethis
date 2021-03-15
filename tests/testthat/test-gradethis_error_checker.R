426351142e28dd3d1ef6a361a3d7c08f3821ddc7
Garrick Aden-Buie <garrick@adenbuie.com>
2021-03-15 13:55:28 -0400

    refactor: global shenanigans strike again?

--- tests/testthat/test-gradethis_error_checker.R
+++ tests/testthat/test-gradethis_error_checker.R
@@ -1,17 +1,17 @@
 test_that("default error checker returns user error message", {
   feedback <- 
     expect_exercise_checker(
-      "b",
+      "definitely_not_right",
       check_code = "gradethis_error_checker()",
       is_correct = FALSE,
-      msg = I("object 'b' not found")
+      msg = I("object 'definitely_not_right' not found")
     )
 })
 
 test_that("default error checker returns code feedback", {
   feedback <- 
     expect_exercise_checker(
-      "b",
+      "definitely_not_right",
       solution_code = "a",
       check_code = "gradethis_error_checker()",
       is_correct = FALSE,
