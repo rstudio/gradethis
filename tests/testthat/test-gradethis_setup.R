73d002e52692d7afe26141976e544a7201edfa9f
Garrick Aden-Buie <garrick@adenbuie.com>
2021-09-20 12:28:57 -0400

    One more test for expected useage of `gradethis_setup()`

--- tests/testthat/test-gradethis_setup.R
+++ tests/testthat/test-gradethis_setup.R
@@ -1,3 +1,19 @@
+test_that("gradethis_setup() sets `grading_problem.type` and `grading_problem.message`", {
+  with_options(
+    list(
+      gradethis.grading_problem.type = NULL,
+      gradethis.grading_problem.message = NULL
+    ), {
+      gradethis_setup(
+        grading_problem.type = "info", 
+        grading_problem.message = "TEST PASS"
+      )
+      expect_equal(gradethis_settings$grading_problem.type(), "info")
+      expect_equal(gradethis_settings$grading_problem.message(), "TEST PASS")
+    }
+  )
+})
+
 test_that("gradethis_setup() issues warning for invalid `grading_problem.type`", {
   with_options(list(gradethis.grading_problem.type = NULL), {
     testthat::expect_message(
