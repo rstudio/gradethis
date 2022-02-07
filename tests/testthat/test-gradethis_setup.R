d79a336a9715b52ef1ad91e4dd7eb9e58a537539
Garrick Aden-Buie <garrick@adenbuie.com>
2022-02-07 10:42:00 -0500

    gradethis_setup() should set exercise.checker every time

--- tests/testthat/test-gradethis_setup.R
+++ tests/testthat/test-gradethis_setup.R
@@ -33,3 +33,24 @@ test_that("gradethis_setup() issues warning for invalid `grading_problem.type`",
     )
   })
 })
+
+test_that("gradethis_setup() sets default learnr options via chunk opts", {
+  local_knitr_opts_chunk()
+  
+  with_knitr_opts_chunk(list(exercise.checker = "fail"), {
+    gradethis_setup()
+    expect_equal(
+      knitr::opts_chunk$get("exercise.checker"), 
+      gradethis_exercise_checker
+    )
+  })
+  
+  with_knitr_opts_chunk(list(exercise.checker = "fail"), {
+    gradethis_setup(exercise.timelimit = 42)
+    expect_equal(
+      knitr::opts_chunk$get("exercise.checker"), 
+      gradethis_exercise_checker
+    )
+    expect_equal(knitr::opts_chunk$get("exercise.timelimit"), 42)
+  })
+})
