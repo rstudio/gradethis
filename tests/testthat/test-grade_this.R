1c32074a3f1914586b6e699b60fde142935792b8
Garrick Aden-Buie <garrick@adenbuie.com>
2022-02-11 15:49:53 -0500

    Wrap grading logic in a function to enable `return()`

---
+++ tests/testthat/test-grade_this.R
@@ -0,0 +1,19 @@
+test_that("grade_this() can find objects in the original env where it was defined", {
+  from_calling_env <- "CALLER"
+  grader <- grade_this({
+    return(from_calling_env)
+  })
+  
+  expect_equal(
+    grader(mock_this_exercise(1)),
+    "CALLER"
+  )
+})
+
+test_that("grade_this() can return early", {
+  expect_null(
+    grade_this({
+      return()
+    })(mock_this_exercise(1))
+  )
+})
