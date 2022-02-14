a4f4704f29589396ca5447cd2c74cb855e8d9e44
Garrick Aden-Buie <garrick@adenbuie.com>
2022-02-14 16:47:37 -0500

    Add env_rls() helper function

--- R/utils.R
+++ R/utils.R
@@ -41,6 +41,16 @@ deparse_to_string <- function(x, width.cutoff = 500L, ...) {
   paste0(deparse(x, width.cutoff = width.cutoff, ...), collapse = "\n")
 }
 
+env_rls <- function(env) {
+  if (identical(env, rlang::global_env()) || rlang::is_namespace(env)) {
+    rlang::env_print(env)
+    return(invisible(env))
+  }
+  purrr::walk(
+    c(list(env), rlang::env_parents(env)),
+    rlang::env_print
+  )
+}
 
 is_tag_like <- function(x) {
   inherits(x, c("shiny.tag", "shiny.tag.list"))
