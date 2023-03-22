1fe61ce7e73e3c3d927a3c262593a30871b0031f
Alex Rossell Hayes <alexander@rossellhayes.com>
2023-03-22 13:32:30 -0700

    feat: add `solution_object_get()`, `solution_object_exists()` and `solution_object_list()`

--- R/user_object.R
+++ R/user_object.R
@@ -1,19 +1,21 @@
-#' Functions for interacting with objects created by student code
+#' Functions for interacting with objects created by student and solution code
 #'
 #' @example man/examples/example-user_object.R
 #'
 #' @inheritParams utils::ls.str
 #' @param x An object name, given as a quoted [character] string.
-#' @param envir An [environment] in which to search for objects.
+#' @param envir An [environment]
+#'   in which to search for objects created by student code.
 #'   Defaults to [`.envir_result`].
 #' @param exclude_envir An [environment].
-#'   Objects that appear in both `envir` and `exclude_envir` will be excluded.
+#'   Objects that appear in `exclude_envir` will be excluded from results.
 #'   Defaults to [`.envir_prep`].
 #'   Use `exclude_envir = NULL` to include all objects.
 #' @param ... Additional arguments passed to underlying functions:
-#'   - For `user_object_exists()`, [exists()]
-#'   - For `user_object_get()`, [get()]
-#'   - For `user_object_list()`, [`ls.str()`][utils::ls.str]
+#'   - For `user_object_exists()` and `solution_object_exists()`, [exists()]
+#'   - For `user_object_get()` and `solution_object_get()`, [get()]
+#'   - For `user_object_list()` and `solution_object_list()`,
+#'     [`ls.str()`][utils::ls.str]
 #' @param gradethis_env The [environment] from which to retrieve
 #'   [`.envir_result`] and [`.envir_prep`].
 #'   Most users of \pkg{gradethis} will not need to use this argument.
@@ -23,7 +25,7 @@ user_object <- function(x, mode, envir, exclude_envir, ..., gradethis_env) NULL
 
 #' @rdname user_object
 #' @export
-#' @return For `user_object_get()`, the object.
+#' @return For `user_object_get()` and `solution_object_get()`, the object.
 #'   If the object is not found, an error.
 user_object_get <- function(
   x, mode = "any", envir = .envir_result, ..., gradethis_env = parent.frame()
@@ -34,7 +36,8 @@ user_object_get <- function(
 
 #' @rdname user_object
 #' @export
-#' @return For `user_object_exists()`, a [`TRUE`]/[`FALSE`] value.
+#' @return For `user_object_exists()` and `solution_object_exists()`,
+#'   a [`TRUE`]/[`FALSE`] value.
 user_object_exists <- function(
   x, mode = "any", envir = .envir_result, ..., gradethis_env = parent.frame()
 ) {
@@ -44,8 +47,9 @@ user_object_exists <- function(
 
 #' @rdname user_object
 #' @export
-#' @return For `user_object_list()`, a [character] vector
-#'   giving the names of the objects in `envir`.
+#' @return For `user_object_list()` and `solution_object_list()`,
+#'   a [character] vector giving the names of the objects
+#'   created by the student or solution code.
 user_object_list <- function(
   mode = "any",
   envir = .envir_result,
@@ -69,4 +73,44 @@ user_object_list <- function(
   }
 
   objects
+}
+
+#' @rdname user_object
+#' @export
+solution_object_get <- function(
+  x, mode = "any", ..., gradethis_env = parent.frame()
+) {
+  resolve_placeholder(.solution, gradethis_env)
+  user_object_get(
+    x, mode = mode, envir = .envir_solution, ..., gradethis_env = gradethis_env
+  )
+}
+
+#' @rdname user_object
+#' @export
+solution_object_exists <- function(
+  x, mode = "any", ..., gradethis_env = parent.frame()
+) {
+  resolve_placeholder(.solution, gradethis_env)
+  user_object_exists(
+    x, mode = mode, envir = .envir_solution, ..., gradethis_env = gradethis_env
+  )
+}
+
+#' @rdname user_object
+#' @export
+solution_object_list <- function(
+  mode = "any",
+  exclude_envir = .envir_prep,
+  ...,
+  gradethis_env = parent.frame()
+) {
+  resolve_placeholder(.solution, gradethis_env)
+  user_object_list(
+    mode = mode,
+    envir = .envir_solution,
+    exclude_envir = exclude_envir,
+    ...,
+    gradethis_env = gradethis_env
+  )
 }
--- man/examples/example-user_object.R
+++ man/examples/example-user_object.R
@@ -1,41 +1,51 @@
-user_code <- rlang::expr({
+user_code <- quote({
   # ```{r example}
-  x <- 1
+  x <- "I'm student code!"
   y <- list(1, 2, 3)
   z <- function() print("Hello World!")
   # ```
 })
 
-exercise <- mock_this_exercise(!!user_code)
-.envir_result <- exercise$.envir_result
-.envir_prep <- exercise$.envir_prep
+solution_code <- quote({
+  # ```{r example-solution}
+  x <- "I'm solution code!"
+  y <- list("a", "b", "c")
+  z <- function() print("Goodnight Moon!")
+  # ```
+})
 
-user_object_list()
-user_object_exists("x")
-user_object_get("y")
+exercise <- mock_this_exercise(!!user_code, !!solution_code)
+
+with_exercise(exercise, user_object_list())
+with_exercise(exercise, user_object_exists("x"))
+with_exercise(exercise, user_object_get("x"))
+
+with_exercise(exercise, solution_object_list())
+with_exercise(exercise, solution_object_exists("x"))
+with_exercise(exercise, solution_object_get("x"))
 
 # Use `mode` to find only objects of a certain type ----
-user_object_list(mode = "numeric")
-user_object_list(mode = "list")
-user_object_list(mode = "function")
+with_exercise(exercise, user_object_list(mode = "character"))
+with_exercise(exercise, user_object_list(mode = "list"))
+with_exercise(exercise, user_object_list(mode = "function"))
 
-user_object_exists("x", mode = "numeric")
-user_object_exists("y", mode = "numeric")
+with_exercise(exercise, user_object_exists("x", mode = "character"))
+with_exercise(exercise, user_object_exists("y", mode = "character"))
 
-user_object_get("z", mode = "function")
+with_exercise(exercise, user_object_get("z", mode = "function"))
 
 # By default, `user_object_list()` ignores objects created by setup chunks ----
 setup_code <- rlang::expr({
   # ```{r example-setup}
-  exercise_data <- mtcars
+  setup_data <- mtcars
   # ```
 })
 
-exercise <- mock_this_exercise(!!user_code, setup_exercise = !!setup_code)
-.envir_result <- exercise$.envir_result
-.envir_prep <- exercise$.envir_prep
+setup_exercise <- mock_this_exercise(
+  !!user_code, !!solution_code, setup_exercise = !!setup_code
+)
 
-user_object_list()
+with_exercise(setup_exercise, user_object_list())
 
 ## You can disable this by setting `exclude_envir = NULL` ----
-user_object_list(exclude_envir = NULL)
+with_exercise(setup_exercise, user_object_list(exclude_envir = NULL))
