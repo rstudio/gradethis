7da574338603743bec3c2ba40f87f27c833fb8bc
Alex Rossell Hayes <44556601+rossellhayes@users.noreply.github.com>
2023-06-02 12:23:11 -0700

    Only use `gradethis_equal.list()` method if both objects are bare lists (#357)

--- tests/testthat/test-gradethis_equal.R
+++ tests/testthat/test-gradethis_equal.R
@@ -2,6 +2,24 @@ test_that("gradethis_equal.list() checks names", {
   expect_false(gradethis_equal(list(pi, letters), list(a = pi, b = letters)))
 })
 
+test_that("gradethis_equal.list() is only used when x and y are both bare lists", {
+  example_list <- list(1, 2, 3)
+  example_vector <- c(1, 2, 3)
+
+  expect_false(gradethis_equal(example_list, example_vector))
+  expect_false(gradethis_equal(example_vector, example_list))
+  expect_false(gradethis_equal(list(example_list), list(example_vector)))
+  expect_false(gradethis_equal(list(example_vector), list(example_list)))
+
+  example_list <- list(a = 1, b = 2)
+  example_data_frame <- data.frame(a = 1, b = 2)
+
+  expect_false(gradethis_equal(example_list, example_data_frame))
+  expect_false(gradethis_equal(example_data_frame, example_list))
+  expect_false(gradethis_equal(list(example_list), list(example_data_frame)))
+  expect_false(gradethis_equal(list(example_data_frame), list(example_list)))
+})
+
 test_that("gradethis_equal uses methods from ggcheck", {
   skip_if_not_installed("ggcheck", "0.0.5")
   skip_if_not_installed("ggplot2")
