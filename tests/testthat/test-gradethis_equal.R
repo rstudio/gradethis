55d11d8ee8f988fc2482a3db4b7d3131807ee589
Alex Rossell Hayes <alexander@rossellhayes.com>
2023-06-02 11:54:43 -0700

    test(gradethis_equal.list): add tests

--- tests/testthat/test-gradethis_equal.R
+++ tests/testthat/test-gradethis_equal.R
@@ -2,11 +2,22 @@ test_that("gradethis_equal.list() checks names", {
   expect_false(gradethis_equal(list(pi, letters), list(a = pi, b = letters)))
 })
 
-test_that("gradethis_equal() distinguishes lists from vectors", {
-  expect_false(gradethis_equal(as.list(letters), letters))
-  expect_false(gradethis_equal(letters, as.list(letters)))
-  expect_false(gradethis_equal(list(as.list(letters)), list(letters)))
-  expect_false(gradethis_equal(list(letters), list(as.list(letters))))
+test_that("gradethis_equal.list() is only used when x and y are both bare lists", {
+  example_list <- list(1, 2, 3)
+  example_vector <- c(1, 2, 3)
+
+  expect_false(gradethis_equal(example_list, example_vector))
+  expect_false(gradethis_equal(example_vector, example_list))
+  expect_false(gradethis_equal(list(example_list), list(example_vector)))
+  expect_false(gradethis_equal(list(example_vector), list(example_list)))
+
+  example_list <- list(a = 1, b = 2)
+  example_data_frame <- data.frame(a = 1, b = 2)
+
+  expect_false(gradethis_equal(example_list, example_data_frame))
+  expect_false(gradethis_equal(example_data_frame, example_list))
+  expect_false(gradethis_equal(list(example_list), list(example_data_frame)))
+  expect_false(gradethis_equal(list(example_data_frame), list(example_list)))
 })
 
 test_that("gradethis_equal uses methods from ggcheck", {
